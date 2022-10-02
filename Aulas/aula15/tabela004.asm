0e35455b65b242464b20e3f63f71538b7870e58a
_Biel Silva_ <68821856+Biel-Silva@users.noreply.github.com>
2022-10-01 22:15:10 -0300

    Update tabela004.asm

--- Aulas/aula15/tabela004.asm
+++ Aulas/aula15/tabela004.asm
@@ -1,8 +1,18 @@
 section .data
+    msg db 'Hello World!
+    tam equ $- msg
 
 section .text
 
 global _start
 
 _start:
-    mov eax, 1
+    mov eax, 0x4
+    mov ebx, 0x1
+    mov ecx, msg
+    mov adx, tam
+    int 0x80
+
+    mov eax, 0x1
+    mov ebx, 0x0
+    int 0x80
